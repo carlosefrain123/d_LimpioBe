2f1a1ed4c95b7cd356e9c5eaf425cc9f35ee402a
Efrain <carlosefrainchallo1@gmail.com>
2025-05-15 12:25:46 -0500

    Se agrego el template en la app.blade.php y se creo la carpeta partials en donde esta el archivo js.blade.php

---
+++ resources/views/partials/js.blade.php
@@ -0,0 +1,46 @@
+<!-- latest jquery-->
+    <script src="../assets/js/jquery-3.6.0.min.js"></script>
+
+    <!-- jquery ui-->
+    <script src="../assets/js/jquery-ui.min.js"></script>
+
+    <!-- Bootstrap js-->
+    <script src="../assets/js/bootstrap/bootstrap.bundle.min.js"></script>
+    <script src="../assets/js/bootstrap/popper.min.js"></script>
+
+    <!-- feather icon js-->
+    <script src="../assets/js/feather/feather.min.js"></script>
+    <script src="../assets/js/feather/feather-icon.js"></script>
+
+    <!-- Lazyload Js -->
+    <script src="../assets/js/lazysizes.min.js"></script>
+
+    <!-- Slick js-->
+    <script src="../assets/js/slick/slick.js"></script>
+    <script src="../assets/js/bootstrap/bootstrap-notify.min.js"></script>
+    <script src="../assets/js/slick/custom_slick.js"></script>
+
+    <!-- Auto Height Js -->
+    <script src="../assets/js/auto-height.js"></script>
+
+    <!-- Quantity Js -->
+    <script src="../assets/js/quantity.js"></script>
+
+    <!-- Timer Js -->
+    <script src="../assets/js/timer1.js"></script>
+    <script src="../assets/js/timer2.js"></script>
+    <script src="../assets/js/timer3.js"></script>
+    <script src="../assets/js/timer4.js"></script>
+
+    <!-- Fly Cart Js -->
+    <script src="../assets/js/fly-cart.js"></script>
+
+    <!-- WOW js -->
+    <script src="../assets/js/wow.min.js"></script>
+    <script src="../assets/js/custom-wow.js"></script>
+
+    <!-- script js -->
+    <script src="../assets/js/script.js"></script>
+
+    <!-- theme setting js -->
+    <script src="../assets/js/theme-setting.js"></script>
