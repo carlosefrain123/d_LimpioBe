02eb62de32290d4bd82d5474cd06ca76d9098119
Efrain <carlosefrainchallo1@gmail.com>
2025-05-17 15:43:01 -0500

    Se edita el footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,10 +1,65 @@
 <footer class="section-t-space footer-section-2">
     <div class="container-fluid-lg">
+
+        <div class="service-section">
+            <div class="row g-3">
+
+                <div class="col-12">
+                    <div class="service-contain">
+                        <!-- Producto siempre fresco -->
+                        <div class="service-box">
+                            <div class="service-image">
+                                <img src="../assets/svg/product.svg" class="blur-up lazyload" alt="Productos frescos">
+                            </div>
+
+                            <div class="service-detail">
+                                <h5>Productos Siempre Frescos</h5>
+                            </div>
+                        </div>
+
+                        <!-- Envío gratuito -->
+                        <div class="service-box">
+                            <div class="service-image">
+                                <img src="../assets/svg/delivery.svg" class="blur-up lazyload" alt="Envío gratis">
+                            </div>
+
+                            <div class="service-detail">
+                                <h5>Envío Gratis en Compras Mayores a $50</h5>
+                            </div>
+                        </div>
+
+                        <!-- Descuentos diarios -->
+                        <div class="service-box">
+                            <div class="service-image">
+                                <img src="../assets/svg/discount.svg" class="blur-up lazyload" alt="Descuentos diarios">
+                            </div>
+
+                            <div class="service-detail">
+                                <h5>Descuentos Especiales Todos los Días</h5>
+                            </div>
+                        </div>
+
+                        <!-- Mejor precio del mercado -->
+                        <div class="service-box">
+                            <div class="service-image">
+                                <img src="../assets/svg/market.svg" class="blur-up lazyload" alt="Mejor precio">
+                            </div>
+
+                            <div class="service-detail">
+                                <h5>El Mejor Precio del Mercado</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
         <div class="main-footer">
             <div class="row g-md-4 gy-sm-5 gy-2">
                 <div class="col-xxl-3 col-xl-4 col-sm-6">
-                    <a href="index.html" class="foot-logo">
-                        <img src="../assets/images/logo/3.png" class="img-fluid" alt="">
+                    <a href="#" class="foot-logo">
+                        <img src="{{ asset('assets/images/logo/3.png') }}" class="img-fluid" alt="" style="width: 75px;">
                     </a>
                     <p class="information-text">it is a long established fact that a reader will be distracted
                         by the readable content.</p>
@@ -35,105 +90,18 @@
                             </a>
                         </li>
                     </ul>
-
-                    <div class="social-app mt-sm-4 mt-3 mb-4">
-                        <ul>
-                            <li>
-                                <a href="https://play.google.com/store/apps" target="_blank">
-                                    <img src="../assets/images/playstore.svg" class="blur-up lazyload" alt="">
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://www.apple.com/in/app-store/" target="_blank">
-                                    <img src="../assets/images/appstore.svg" class="blur-up lazyload" alt="">
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
                 </div>
 
                 <div class="col-xxl-2 col-xl-4 col-sm-6">
-                    <div class="footer-title">
-                        <h4>About Fastkart</h4>
-                    </div>
-                    <ul class="footer-list footer-contact mb-sm-0 mb-3">
-                        <li>
-                            <a href="about-us.html" class="footer-contain-2">
-                                <i class="fas fa-angle-right"></i>About Us</a>
-                        </li>
-                        <li>
-                            <a href="contact-us.html" class="footer-contain-2">
-                                <i class="fas fa-angle-right"></i>Contact Us</a>
-                        </li>
-                        <li>
-                            <a href="term_condition.html" class="footer-contain-2">
-                                <i class="fas fa-angle-right"></i>Terms & Coditions</a>
-                        </li>
-                        <li>
-                            <a href="careers.html" class="footer-contain-2">
-                                <i class="fas fa-angle-right"></i>Careers</a>
-                        </li>
-                        <li>
-                            <a href="blog-list.html" class="footer-contain-2">
-                                <i class="fas fa-angle-right"></i>Latest Blog</a>
-                        </li>
-                    </ul>
+
                 </div>
 
                 <div class="col-xxl-2 col-xl-4 col-sm-6">
-                    <div class="footer-title">
-                        <h4>Useful Link</h4>
-                    </div>
-                    <ul class="footer-list footer-contact mb-sm-0 mb-3">
-                        <li>
-                            <a href="order-success.html" class="footer-contain-2">
-                                <i class="fas fa-angle-right"></i>Your Order</a>
-                        </li>
-                        <li>
-                            <a href="user-dashboard.html" class="footer-contain-2">
-                                <i class="fas fa-angle-right"></i>Your Account</a>
-                        </li>
-                        <li>
-                            <a href="order-tracking.html" class="footer-contain-2">
-                                <i class="fas fa-angle-right"></i>Track Orders</a>
-                        </li>
-                        <li>
-                            <a href="wishlist.html" class="footer-contain-2">
-                                <i class="fas fa-angle-right"></i>Your Wishlist</a>
-                        </li>
-                        <li>
-                            <a href="faq.html" class="footer-contain-2">
-                                <i class="fas fa-angle-right"></i>FAQs</a>
-                        </li>
-                    </ul>
+
                 </div>
 
                 <div class="col-xxl-2 col-xl-4 col-sm-6">
-                    <div class="footer-title">
-                        <h4>Categories</h4>
-                    </div>
-                    <ul class="footer-list footer-contact mb-sm-0 mb-3">
-                        <li>
-                            <a href="vegetables-demo.html" class="footer-contain-2">
-                                <i class="fas fa-angle-right"></i>Fresh Vegetables</a>
-                        </li>
-                        <li>
-                            <a href="spice-demo.html" class="footer-contain-2">
-                                <i class="fas fa-angle-right"></i>Hot Spice</a>
-                        </li>
-                        <li>
-                            <a href="bags-demo.html" class="footer-contain-2">
-                                <i class="fas fa-angle-right"></i>Brand New Bags</a>
-                        </li>
-                        <li>
-                            <a href="bakery-demo.html" class="footer-contain-2">
-                                <i class="fas fa-angle-right"></i>New Bakery</a>
-                        </li>
-                        <li>
-                            <a href="grocery-demo.html" class="footer-contain-2">
-                                <i class="fas fa-angle-right"></i>New Grocery</a>
-                        </li>
-                    </ul>
+
                 </div>
 
                 <div class="col-xxl-3 col-xl-4 col-sm-6">
@@ -141,15 +109,6 @@
                         <h4>Store infomation</h4>
                     </div>
                     <ul class="footer-address footer-contact">
-                        <li>
-                            <a href="javascript:void(0)">
-                                <div class="inform-box flex-start-box">
-                                    <i data-feather="map-pin"></i>
-                                    <p>Fastkart Demo Store, Demo store india 345 - 659</p>
-                                </div>
-                            </a>
-                        </li>
-
                         <li>
                             <a href="javascript:void(0)">
                                 <div class="inform-box">
@@ -167,15 +126,6 @@
                                 </div>
                             </a>
                         </li>
-
-                        <li>
-                            <a href="javascript:void(0)">
-                                <div class="inform-box">
-                                    <i data-feather="printer"></i>
-                                    <p>Fax: 123456</p>
-                                </div>
-                            </a>
-                        </li>
                     </ul>
                 </div>
             </div>
@@ -183,24 +133,12 @@
 
         <div class="sub-footer section-small-space">
             <div class="left-footer">
-                <p>2022 Copyright By Themeforest Powered By Pixelstrap</p>
+                <p>2025 Copyright By AnderCode</p>
             </div>
             <div class="right-footer">
                 <ul class="payment-box">
                     <li>
                         <img src="../assets/images/icon/paymant/visa.png" alt="">
-                    </li>
-                    <li>
-                        <img src="../assets/images/icon/paymant/discover.png" alt="">
-                    </li>
-                    <li>
-                        <img src="../assets/images/icon/paymant/american.png" alt="">
-                    </li>
-                    <li>
-                        <img src="../assets/images/icon/paymant/master-card.png" alt="">
-                    </li>
-                    <li>
-                        <img src="../assets/images/icon/paymant/giro-pay.png" alt="">
                     </li>
                 </ul>
             </div>
