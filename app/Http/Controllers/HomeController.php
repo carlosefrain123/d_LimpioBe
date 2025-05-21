df4644e3c324a9109f886afac813c521954091f0
Efrain <carlosefrainchallo1@gmail.com>
2025-05-21 12:24:06 -0500

    Solo para recordar que en HomeController se puede modificar la cantidad productos que se mostróy al momento de darle el corazón te muestra un mensaje

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends Controller
     public function index() {
         $topSellingProducts = Product::withCount('orderItems') // Contar la cantidad de veces que un producto se vendió
             ->orderByDesc('order_items_count') // Ordenar por los más vendidos
-            ->take(12) // Tomar solo 24 productos
+            ->take(12) // Tomar solo 24 productos - Acá se cambia la cantidad de productos que se cambia en el TOP PRODUCTOS
             ->get();
 
         return view('welcome', compact('topSellingProducts'));
